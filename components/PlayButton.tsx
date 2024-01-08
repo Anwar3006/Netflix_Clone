7c736bd7b920599ad4c2f26fe7b57a7738c587a7
Anwar3006 <anwarsadat.d2@gmail.com>
2024-01-08 16:55:11 +0000

    no change

--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -13,7 +13,7 @@ const PlayButton: React.FC<PlayButtonProps> = ({ movieId }) => {
     <button
       onClick={() => router.push(`/watch/${movieId}`)}
       className="bg-white rounded-md py-1 md:py-2 px-2 md:px-4 text-xs lg:text-lg 
-                    font-semibold flex flex-row items-center transition hover:bg-neutral-300">
+                    font-semibold flex flex-row items-center transition hover:bg-neutral-400">
       <BsFillPlayFill size={30} />
       Play
     </button>
